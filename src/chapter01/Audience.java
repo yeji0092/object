7169f91ee84b4e599ed4a3367725f49a551afafe
yeji <yeji0092@wisenut.co.kr>
2019-10-25 14:41:59 +0900

    Audience 캡슐화 Bag와 TicketSeller의 의존성 제거 => 결합도 낮춤

--- src/chapter01/Audience.java
+++ src/chapter01/Audience.java
@@ -8,7 +8,20 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    /*
+    Bag를 외부에 반환하지 않고 내부에서 직접 접근하여 티켓 구매를 수행하므로
+    getBag 메소드 삭제
+    Bag에 대한 접근을 캡슐화하여 TicketSeller와 Bag사이의 결합도를 낮춤
+     */
+    public Long buyTicket(Ticket ticket){
+        if(bag.hasInvitaion()){//초대장 소지하고 있는 관람객
+            bag.setTicket(ticket);
+            return 0L;
+            //초대장 삭제하는 로직?
+        }else{
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
--- src/chapter01/TicketSeller.java
+++ src/chapter01/TicketSeller.java
@@ -10,20 +10,12 @@ public class TicketSeller {
 
     /*
     티켓 판매원이 직접 자율적으로 티켓을 판매하도록 sellTo 메소드에 로직 추가
-    ticketOffice를 외부에 반환하지 않고 내부에서 직접 접근하여 티켓판매를 수행하므로
+    TicketOffice를 외부에 반환하지 않고 내부에서 직접 접근하여 티켓판매를 수행하므로
     getTicketOffice 메소드 삭제
     ticketOffice에 대한 접근을 캡슐화(객체 내부의 세부사항을 감추는 일)하여 객체(theater)와 객체(ticketOffice) 사이의 결합도를 낮춤
      */
     public void sellTo(Audience audience){
-        if(audience.getBag().hasInvitaion()){ //초대장 있는 관람객
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-            //관람객 초대장 삭제하는 로직도 필요
-        }else{
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        Long fee = audience.buyTicket(ticketOffice.getTicket());
+        ticketOffice.plusAmount(fee);
     }
 }
