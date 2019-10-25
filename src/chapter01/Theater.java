31871f3ab3fceaa4e532bbd00c3ea8174325b7fe
yeji <yeji0092@wisenut.co.kr>
2019-10-25 11:21:21 +0900

    TicketSeller 캡슐화 Theater와 TicketOffice의 의존성 제거 => 결합도 낮춤

--- src/chapter01/Theater.java
+++ src/chapter01/Theater.java
@@ -8,15 +8,12 @@ public class Theater {
     }
     //입장하는 로직
     public void enter(Audience audience){
-        if(audience.getBag().hasInvitaion()){ //초대장 있는 관람객
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-            //초대장을 없애주는 로직은?
-        }else{ //초대장 없는 관람객-> 돈주고 티켓 사야함
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        /*
+        입장 시, 티켓이 필요한 로직은 티켓판매원이 자율적으로 하도록 변경
+        Theater에서는 더이상 ticketOffice에 접근하지 못함
+        ticketOffice가 ticketSeller 내부에 존재하는 사실을 알지 못함
+        =>캡슐화(객체 내부의 세부사항을 감추는 일)
+         */
+        ticketSeller.sellTo(audience);
     }
 }
